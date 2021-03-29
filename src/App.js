a2e1fc7f9dabda52e47b25ae9207766886658a24
dragon-san <66186078+dragon-san@users.noreply.github.com>
2021-03-30 05:25:42 +0530

    i

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React, { Component } from "react";
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+
 import './App.css';
-// import Products from './Components/products';
 
-// import Home from './Components/home';
-class App {
-  render() {
-    return (
 
-      <div className="App">
-        {/* <Router>
-<Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/products' exact component={Products}/>
-        
-        
-        </Switch>
+import Home from './Components/home';
 
-</Router> */}
-<h1> HELLO WORLD</h1>
-      </div>
-    );
-  }
+function App(){
+return(
+  <div className="App">
+    HELLO WORLD
+    </div>
+
+);
+
 }
+
 
 export default App;
